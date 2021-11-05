ec65c680ad7a07242044ea3e83c7f201fbe48487
Marcin Oleś <marcin.oles@deployed.pl>
2021-11-05 11:48:24 +0100

    create first model

--- ghactions/app/models.py
+++ ghactions/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(default=5)
+
+    def __str__(self):
+        return f"{self.name} {self.age}"
+
+
