e7d2037f962f37554be6c930fd6518d828f16dcd
Marcin Oleś <marcin.oles@deployed.pl>
2021-11-06 20:47:36 +0100

    run ssh agent

--- deploy/base_fabfile.py
+++ deploy/base_fabfile.py
@@ -375,6 +375,7 @@ class Deployment(object):
         set_permissions()
         with warn_only():
             run("echo $SSH_AUTH_SOCK")
+            run("ssh-agent")
             run("ssh -T git@github.com -vvv")
             run("ssh-add -L")
         fetch(branch_name=branch)
