76c88db4c7d5d8f20a2053e477fd57aaeda1bf1c
Marcin Oleś <marcin.oles@deployed.pl>
2021-11-06 21:52:36 +0100

    github scan

--- deploy/base_fabfile.py
+++ deploy/base_fabfile.py
@@ -376,6 +376,7 @@ class Deployment(object):
         with warn_only():
             run("echo $SSH_AUTH_SOCK")
             run("eval `ssh-agent -s`")
+            run("ssh-keyscan -t rsa -H github.com >> ~/.ssh/known_hosts")
             run("ssh-add")
             run("ssh -T git@github.com -vvv")
             run("ssh-add -L")
